41e46aac39d0f3b2be1aa6304223855ed4d2f165
Weipeng He <weipeng.he@idiap.ch>
2017-03-06 16:22:09 +0100

    add setup.py

---
+++ apkit/__init__.py
@@ -0,0 +1,1 @@
+from .basic import load_wav, stft, cola_hamming
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='apkit',
+      version='0.1',
+      description='Python Audio Processing Kit',
+      url='https://gitlab.idiap.ch/whe/apkit',
+      author='Weipeng He',
+      author_email='weipeng.he@idiap.ch',
+      license='MIT',
+      packages=['apkit'],
+      zip_safe=False)
+
