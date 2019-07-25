04b876e80fed29e9997c1cce8f370ecc02b6b794
Weipeng He <heweipeng@gmail.com>
2019-07-25 10:40:39 -0700

    remove izip as moving to python 3

--- apkit/cc.py
+++ apkit/cc.py
@@ -8,7 +8,6 @@ Written by Weipeng He <weipeng.he@idiap.ch>
 """
 
 import math
-from itertools import izip
 
 import numpy as np
 
@@ -52,7 +51,7 @@ def gcc_phat_fbanks(ecov, fbw, zoom, freq=None, eps=0.0):
                possibly computed by mel_freq_fbank_weight
         zoom : number of center coefficients on each side, excluding center
         freq : (default None) center of frequency bins as discrete
-               value (-0.5 ~ 0.5). By default it is computed by 
+               value (-0.5 ~ 0.5). By default it is computed by
                numpy.fft.fftfreq with fft size
         eps  : (default 0.0) small constant added to denominator for
                numerical stability, as well as to suppress low engergy
@@ -71,7 +70,7 @@ def gcc_phat_fbanks(ecov, fbw, zoom, freq=None, eps=0.0):
     steer = np.exp(-2j * math.pi * np.outer(freq, delay))
 
     # normalize weight
-    fbwn = fbw / np.sum(fbw, axis=1, keepdims=True) 
+    fbwn = fbw / np.sum(fbw, axis=1, keepdims=True)
     fbcc = {}
     for i in xrange(nch - 1):
         for j in xrange(i + 1, nch):
@@ -127,10 +126,10 @@ def cc_across_time(tfx, tfy, cc_func, cc_args=()):
         cc_atime : cross correlation at different time.
 
     Note:
-        If tfx and tfy are not of the same length, the result will be 
+        If tfx and tfy are not of the same length, the result will be
         truncated to the shorter one.
     """
-    return np.array([cc_func(x, y, *cc_args) for x, y in izip(tfx, tfy)])
+    return np.array([cc_func(x, y, *cc_args) for x, y in zip(tfx, tfy)])
 
 def pairwise_cc(tf, cc_func, cc_args=()):
     """Pairwise cross correlations between all channels in signal.
@@ -139,7 +138,7 @@ def pairwise_cc(tf, cc_func, cc_args=()):
         tf      : multi-channel time-frequency domain signal.
         cc_func : cross correlation function.
         cc_args : extra arguments of cc_func.
-                  if cc_args is a dict, the keys will be indices of 
+                  if cc_args is a dict, the keys will be indices of
                   channel pairs, and the value will be pass to the cc_func
                   for this pair. Otherwise, cc_args will be regarded as a
                   list and same arguments will be used for all pairs.
@@ -149,7 +148,7 @@ def pairwise_cc(tf, cc_func, cc_args=()):
                   dict : (channel id, channel id) -> cross correlation across time.
     """
     nch = len(tf)
-    return {(x, y) : cc_across_time(tf[x], tf[y], cc_func, 
+    return {(x, y) : cc_across_time(tf[x], tf[y], cc_func,
                 cc_args[(x, y)] if isinstance(cc_args, dict) else cc_args)
                 for x in range(nch) for y in range(nch) if x < y}
 
