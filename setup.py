46e1996690b266d4668a7bb9c6c9bcf89e88225b
Weipeng He <weipeng.he@idiap.ch>
2020-04-27 21:46:43 +0200

    edit setup information for release

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 from setuptools import setup
 
-setup(name='apkit',
-      version='0.2',
-      description='Python Audio Processing Kit',
-      url='https://github.com/hwp/apkit',
-      author='Weipeng He',
-      author_email='heweipeng@gmail.com',
-      license='MIT',
-      packages=['apkit'],
-      install_requires=['scipy', 'numpy'],
-      include_package_data=True,
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='apkit',
+    version='0.3.1',
+    author='Weipeng He',
+    author_email='heweipeng@gmail.com',
+    description='Python Audio Processing Kit',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/hwp/apkit',
+    license='MIT',
+    packages=['apkit'],
+    install_requires=['scipy', 'numpy'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
 
