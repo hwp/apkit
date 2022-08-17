735f97a0bd4bf4c8ad1f7125a659dfcfd209ab07
Weipeng He <heweipeng@gmail.com>
2022-08-17 09:59:18 -0700

    add compute_coherence.py

---
+++ examples/compute_coherence.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import argparse
+
+import numpy as np
+import scipy.signal
+import apkit
+
+def _complex_coherence(x, y, win_size, fs):
+    freq_bins, csd = scipy.signal.csd(x, y, nperseg=win_size, fs=fs)
+    coherence = csd / np.sqrt(scipy.signal.welch(x, nperseg=win_size, fs=fs)[1].real
+                              * scipy.signal.welch(y, nperseg=win_size, fs=fs)[1].real)
+    return freq_bins, coherence
+
+def main(infile, outfile, win_size, plot_title, plot_max_freq):
+    fs, sig = apkit.load_wav(infile)
+    n_ch = sig.shape[0]
+    for i in range(n_ch - 1):
+        for j in range(i + 1, n_ch):
+            freq_bins, coherence = _complex_coherence(sig[i], sig[j], 
+                                                      win_size=win_size, fs=fs)
+            with open(f'{outfile}_{i}vs{j}', 'w') as f:
+                for freq, x in zip(freq_bins, coherence):
+                    print(f'{freq}\t{x.real}\t{x.imag}', file=f)
+
+    with open(f'{outfile}_plot.gp', 'w') as f:
+        print('set terminal pngcairo size 800,600', file=f)
+        print(f'set xrange [0:{plot_max_freq}]', file=f)
+        print('set xlabel "Frequency"', file=f)
+        print('set yrange [-.5:1]', file=f)
+        print('set ylabel "Coherence"', file=f)
+        print('set grid', file=f)
+        print('set samples 500', file=f)
+        for i in range(n_ch - 1):
+            for j in range(i + 1, n_ch):
+                print(f"set title '{plot_title} -- ch{i} vs ch{j}'", file=f)
+                print(f"set output '{outfile}_{i}vs{j}.png'", file=f)
+                print(f"plot '{outfile}_{i}vs{j}' u 1:2 smooth csplines w l lc 'green' title 'Real', "
+                            "'' u 1:3 smooth csplines w l lc 'red' title 'Imaginary', "
+                            "'' u 1:(sqrt($2**2+$3**2)) smooth csplines w l lc 'blue' dt '.' title 'Magnitude'",
+                      file=f)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='print complex coherence')
+    parser.add_argument('--win-size', '-w', type=int, required=True,
+                        help='frame size')
+    parser.add_argument('--outfile', '-o', type=str, required=True,
+                        help='output files prefix')
+    parser.add_argument('--plot-title', type=str, default='Coherence',
+                        help='plot title')
+    parser.add_argument('--plot-max-freq', type=int, default=1000,
+                        help='max frequency to be plotted')
+    parser.add_argument('infile', metavar='INFILE', type=str,
+                        help='input audio file')
+    args = parser.parse_args()
+    main(args.infile, args.outfile, args.win_size, args.plot_title,
+         args.plot_max_freq)
+
